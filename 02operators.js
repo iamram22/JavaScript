98cc49c7bfbb074e2d1d3266943953ee93317dff
Ramakrishna Kumbar <ramlikestocode@gmail.com>
2024-03-30 21:43:47 +0530

    small practice towards learning basics

---
+++ 02operators.js
@@ -0,0 +1,57 @@
+//Operators : as the name says operators perform perticular operations
+
+//1 Assignment operators "=" followed by " +, -, /, *"
+x = 5;
+y = 10;
+
+a = x * y; 
+b = x + y ;
+c = x * y;
+d = y/x ;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+
+// comparison operator : a logical value (true or false) is returned 
+// " ==, !==, ===, <=, >=, >, <, "
+
+aa = 55;
+bb = "55";
+console.log(aa == bb);
+console.log(aa === bb); // === goes for strict checking and diggs in for data type match
+
+
+var num1 = 7;
+var num2 = 6;
+
+console.log(num1 * num2);
+
+var answer = num1 > num2;
+
+console.log(answer);
+
+var sellingPrice = 199;
+var listingPrice = 799;
+
+var discountPercent = ((listingPrice - sellingPrice)/listingPrice) * 100
+
+// console.log("Discounted Price " + discountPercent);
+
+displayDiscountPercentage = Math.round(discountPercent)
+
+console.log(displayDiscountPercentage + "% off");
+
+console.log(typeof displayDiscountPercentage);
+
+//Logical operators : below explaination from mdn docs, refer 03conditionals for example.
+/* Logical operators are typically used with Boolean (logical) values; 
+when they are, they return a Boolean value. 
+However, the && and || operators actually return the value of one of the specified operands, 
+so if these operators are used with non-Boolean values, 
+they may return a non-Boolean value. The logical operators are described in the following table.*/
+
+
+
+
---
+++ 03conditionals.js
@@ -0,0 +1,82 @@
+//conditionals help us to do check for multiple statements and return a block of code 
+
+
+//1. using if and else
+
+var temperature;
+
+//TODO: go to google and get the data
+
+temperature = 20;
+
+if (temperature < 20) {
+    console.log("It is cold outside");
+}
+if (temperature < 30) {
+    console.log("It is moderate outside");
+} else {
+    console.log("It is hot outside");
+};
+
+
+//another example using logical operators
+
+var email = true;
+var facebook = false;
+var google = false;
+
+if (email || facebook || google) {
+    console.log("Login success");
+}; //&& can also be used to check if all the conditions hold true
+
+//another example using Ternary operator 
+// condition ? run this code : run this code instead
+
+var authenticated = true;
+
+authenticated ? console.log("show dashboard") : console.log("Access denied");
+
+
+//another example using switch and case
+
+var user = "testprep";
+
+switch (user) {
+    case "admin":
+        console.log("You get full access");
+        break;
+    case "subadmin":
+        console.log("You get access create/delete courses");
+        break;
+    case "testprep":
+        console.log("You get the access create/delete tests");
+        break;
+    case "user":
+        console.log("You get full access to consume the content");
+        break;
+
+    default:
+        console.log("Trail User");
+        break;
+};
+
+
+//understanding truthy and falsy
+//null, undefined, 0, "", NaN are falsy values and everything apart from these are truthy values
+
+var user = "2";
+
+if (2 == user) {
+    console.log("Condition is true");
+} else {
+    console.log("Condition is false");
+}
+
+var myUser = "2"
+
+if (2 === myUser) { // === makes strict checking for data types
+    console.log("Condition is true");
+} else {
+    console.log("Condition is false");
+};
+
