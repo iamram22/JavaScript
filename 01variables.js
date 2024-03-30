7cacb4141d1a20e78c342189c3025fec941d1fa6
Ramakrishna Kumbar <ramlikestocode@gmail.com>
2024-03-30 10:51:31 +0530

    done practicing variables

---
+++ 01variables.js
@@ -0,0 +1,37 @@
+//variables can be decalre by using keyword var, let and const and yes they have thier own pusrposes
+
+var myName = Ram;
+
+var satellitesToJupitar = "Io, Europa, Callisto, and many more";
+var satellitesToJupitar = "Aitne, Sponde";
+satellitesToJupitar = "Thebe, Carme";
+//variables decalred using var keyword can be re-decalred and values can be re-assigned.
+
+
+
+let dwarfPlanet = "Pluto";
+// let dwarfPlanet = "Pluto";
+dwarfPlanet = "Yes it is Pluto";
+//varibles decalred using let cannot re-decalred but value can be re-assigned. line no 13 is an INVALID syntax!
+
+
+const theOnlySatelliteToEarth = "Moon";
+// theOnlySatelliteToEarth = "yes it is Moon"; 
+const numberOfSatelliteToEarth = 1;
+//variables decalred using const can neither be re-aasinged not values can be re-decalred
+
+
+const rulesToDecalreVariables = "yes syntax must be follwed";
+
+const ruleNo1 = "Variable name cannot start with a number, can end with a number";
+const ruleNo2 = "Variable name cannot start with symbol, Exception : $ and _ can be used ";
+const ruleNo3 = "Variable name cannot contain spaces";
+const ruleNo4 = "Variable name is case sensetive";
+const ruleNo5 = "Variable name cannot be a reserved keyword, Example: let, while, true, and many more"
+
+let iLearnedAboutVariables = true;
+//variables can also be true or false
+
+
+
+
