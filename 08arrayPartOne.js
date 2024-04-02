3fca401fa965ddf8e8540c3c8e043b81f27422dc
Ramakrishna Kumbar <ramlikestocode@gmail.com>
2024-04-02 23:19:10 +0530

    going good with learning array's

---
+++ 08arrayPartOne.js
@@ -0,0 +1,18 @@
+//array is a collection of data of same or different data types.
+
+var countries = ["India", "Spain", "Japan", "Russia", "France"]
+
+var states = new Array("Karnataka", "Delhi", "Maharastra", "Assam")
+
+console.log(states[2]);
+states[0] = "Himachal Pradesh";
+console.log(states);
+
+var user = ["Ram", "ramlikestocode@gmail.com", 3, 34, true];
+
+// user.pop();
+// user.unshift("New Value")
+// user.shift("New Value")
+// console.log(user);
+
+console.log(user.indexOf(34));
