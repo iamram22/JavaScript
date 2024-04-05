aa150692f384a575453c060813dec74019915e3c
Ramakrishna Kumbar <ramlikestocode@gmail.com>
2024-04-05 19:41:40 +0530

    so far so good with arrays

---
+++ 10arrayPartThree.js
@@ -0,0 +1,16 @@
+const testArray = [2,4,6,3,1,5,9,8]
+
+// console.log(testArray.fill("h", 2));
+
+const myNumber = [1,2,4,5,6,7,8,9,66,75,87,32,78]
+const result = myNumber.filter((num) => num >= 78)
+console.log(result);
+
+var users = ["Ted", "Tim", "Tom", "Sam", "Sor", "Sod"]
+
+console.log(users.slice(1,4));
+
+users.splice(1, 3, "Hie")
+
+console.log(users);
+
