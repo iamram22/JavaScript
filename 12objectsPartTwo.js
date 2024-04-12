e672415ec9e6c657f57d53ee2b641f6a53833854
Ramakrishna Kumbar <ramlikestocode@gmail.com>
2024-04-12 23:04:30 +0530

    learning more about Objects

---
+++ 12objectsPartTwo.js
@@ -0,0 +1,17 @@
+var user = {
+    firstName : "Nibbs",
+    lastName :  "",
+    role : "HR Manager",
+    loginCount : 3,
+    signedInFromPortal : true,
+    signedInFromPhysicalId : false,
+    upComingTasks : [],
+    availableTasks : function (tasks) {
+        this.upComingTasks.push(tasks);
+    },
+    getTasksfromManager : function () {
+        return `${this.firstName} is assigned to complete ${this.upComingTasks.length} number of tasks`
+    },
+};
+
+
