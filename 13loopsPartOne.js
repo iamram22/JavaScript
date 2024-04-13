161578f55f2ea969c76a3dbf08fb72d47a61aad8
Ramakrishna Kumbar <ramlikestocode@gmail.com>
2024-04-13 23:30:41 +0530

    getting started with loops

--- 13loopsPartOne.js
+++ 13loopsPartOne.js
@@ -1,4 +1,5 @@
 // syntax : for(initializer; condition; incrementer/decrementer;)
+// we can also use use k,j,a instead of i, but it is NOT a good programming practice
 
 for(let i = 0; i < 10; i++){
     console.log(i);
