d12ba624b3887b33b2cda0bab299ae4bd8d40ae6
Ramakrishna Kumbar <ramlikestocode@gmail.com>
2024-04-14 23:57:54 +0530

    getting started with loops

--- 13loopsPartOne.js
+++ 13loopsPartOne.js
@@ -5,4 +5,38 @@ for(let i = 0; i < 10; i++){
     console.log(i);
 }
 
+const myStates = ["Delhi",
+ "Karnataka",
+  1947,
+ "Tamil Nadu",
+ "Maharastra",
+ "Punjab"
+];
+
+// for(let i = 0; i < myStates.length; i++) {
+//     if( typeof myStates[i] !== "string") break;
+//     console.log(myStates[i]);
+// }
+
+let i = 0;
+
+// while(i < myStates.length){
+//     console.log(myStates[i]);
+//     i++
+// }
+
+
+// do {
+//     console.log(myStates[i]);
+//     i++;
+// } while(i < myStates.length);
+
+
+//do while loop runs/executes once and checks 
+k = 25
+
+do {
+    console.log(k);
+    k++
+} while(k < 10)
 
