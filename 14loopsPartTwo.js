2ec39932cb7c3c5740ff44acf805d86e28dfda5b
Ramakrishna Kumbar <ramlikestocode@gmail.com>
2024-04-17 23:56:37 +0530

    more practice

---
+++ 14loopsPartTwo.js
@@ -0,0 +1,10 @@
+//foreach is considered as method, rather than loop and it's a loop at the end of the day
+
+const myStates = ["Delhi",
+ "Karnataka",
+  1947,
+ "Tamil Nadu",
+ "Maharastra",
+ "Punjab"
+];
+
