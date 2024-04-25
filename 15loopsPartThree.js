374be4b586d51ec4ec6fc6b71f22b6f371769e84
Ramakrishna Kumbar <ramlikestocode@gmail.com>
2024-04-25 23:56:28 +0530

    a bit more about loops

--- 15loopsPartThree.js
+++ 15loopsPartThree.js
@@ -3,7 +3,20 @@ const names = ["Youtube", "facebook", "Instagram", "Netflix", "Amazon"]
 for(const z of names) {
     console.log(z);
 }
+const symbols = {
+    yt : "Youtube",
+    ig : "Instagram",
+    fb : "facebook",
+    nt : "Netflix",
+    az : "Amazon"
+}
 
+for(const z in symbols) {
+    console.log(symbols[z]);
+}
+
+for (const e in symbols) {
+    console.log(`Key is ${e} and the value is ${symbols[e]}`)
 
 }
 
