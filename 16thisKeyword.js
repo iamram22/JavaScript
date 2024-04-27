379d8c911ec9f80af9244766debf89b508e9d165
Ramakrishna Kumbar <ramlikestocode@gmail.com>
2024-04-27 22:34:24 +0530

    learning about this keyword

--- 16thisKeyword.js
+++ 16thisKeyword.js
@@ -1,2 +1,12 @@
 console.log(this);
 // this will return an empty object
+
+var user = {
+    firstName : "Ramakrishna",
+    courseCount : 34,
+    getCourseCount : function () {
+        console.log("line 7", this);
+    }
+};
+
+user.getCourseCount()
