cad34724940a2d81f270646defdd6fd0a19cdfe4
Ramakrishna Kumbar <ramlikestocode@gmail.com>
2024-04-29 23:31:12 +0530

    few more lines to loops

--- 13loopsPartOne.js
+++ 13loopsPartOne.js
@@ -1,4 +1,5 @@
 // syntax : for(initializer; condition; incrementer/decrementer;)
+// You can think of a loop as a computerized version of the game where you tell someone to take X steps in one direction, then Y steps in another. For example, the idea "Go five steps to the east" could be expressed this way as a loop:
 
 for(let i = 0; i < 10; i++){
     console.log(i);
