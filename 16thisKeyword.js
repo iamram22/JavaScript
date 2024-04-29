6ad887710a1118a606cfde3ba0467b3d007a0395
Ramakrishna Kumbar <ramlikestocode@gmail.com>
2024-04-29 23:28:17 +0530

    more about this keyword

--- 16thisKeyword.js
+++ 16thisKeyword.js
@@ -1,5 +1,6 @@
 console.log(this);
 // this will return an empty object
+//The this keyword refers to the context where a piece of code, such as a function's body, is supposed to run. Most typically, it is used in object methods, where this refers to the object that the method is attached to, thus allowing the same method to be reused on different objects
 
 var user = {
     firstName : "Ramakrishna",
@@ -10,3 +11,7 @@ var user = {
 };
 
 user.getCourseCount()
+
+//Inside a function, the value of this depends on how the function is called. Think about this as a hidden parameter of a function — just like the parameters declared in the function definition, this is a binding that the language creates for you when the function body is evaluated.
+
+// For a regular function (not an arrow function, bound function, etc.), the value of this is the object that the function is accessed on. In other words, if the function call is in the form obj.f(), then this refers to obj. For example
--- 17objectsPartOne.js
+++ 17objectsPartOne.js
@@ -32,4 +32,6 @@ if (meera.hasOwnProperty("firstNamee")) {
 
 //more info from MDN docs : The Object type represents one of JavaScript's data types. It is used to store various keyed collections and more complex entities. Objects can be created using the Object() constructor or the object initializer / literal syntax.
 
+//Objects in JavaScript, just as in many other programming languages, can be compared to objects in real life. In JavaScript, an object is a standalone entity, with properties and type. Compare it with a cup, for example. A cup is an object, with properties. A cup has a color, a design, weight, a material it is made of, etc. The same way, JavaScript objects can have properties, which define their characteristics.
 
+//In addition to objects that are predefined in the browser, you can define your own objects. This chapter describes how to use objects, properties, and methods, and how to create your own objects.
