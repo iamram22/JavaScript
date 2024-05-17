76d4b13bd313bceb5d886202294879878d386ae1
Ramakrishna Kumbar <ramlikestocode@gmail.com>
2024-05-17 12:40:13 +0530

    basics and understanding the importance of parameters

--- 04functionsPartOne.js
+++ 04functionsPartOne.js
@@ -9,7 +9,6 @@ sayHello(); //function outputs only when it is invoked NOT refered
 
 // function can accept parameters.
 
-
 function learnParams(p1, p2, p3){
     console.log(`invoking param1 ${p1}`);
     console.log(`invoking param2 ${p2}`);
