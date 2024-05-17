e81f1da8cf22f194137093a923af694388f35062
Ramakrishna Kumbar <ramlikestocode@gmail.com>
2024-05-17 14:33:10 +0530

    using fill, filter, and splice

--- 10arrayPartThree.js
+++ 10arrayPartThree.js
@@ -1,10 +1,12 @@
 const testArray = [2,4,6,3,1,5,9,8]
 
-// console.log(testArray.fill("h", 2));
+console.table(testArray.fill("h", 2));
+//all the elemets from will  be replace by "h", 
 
 const myNumber = [1,2,4,5,6,7,8,9,66,75,87,32,78]
 const result = myNumber.filter((num) => num >= 78)
 console.log(result);
+
 
 //learning about Splice properties for Arrays
 
