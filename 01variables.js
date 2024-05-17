bbf7015ca902e14e735aa1e07192ff89e4241adf
Ramakrishna Kumbar <ramlikestocode@gmail.com>
2024-05-17 12:53:16 +0530

    Syntax for variables and what to avoid explained

--- 01variables.js
+++ 01variables.js
@@ -8,6 +8,7 @@ satellitesToJupitar = "Thebe, Carme";
 //variables decalred using var keyword can be re-decalred and values can be re-assigned.
 
 
+
 let dwarfPlanet = "Pluto";
 // let dwarfPlanet = "Pluto";
 dwarfPlanet = "Yes it is Pluto";
@@ -17,19 +18,21 @@ dwarfPlanet = "Yes it is Pluto";
 const theOnlySatelliteToEarth = "Moon";
 // theOnlySatelliteToEarth = "yes it is Moon"; 
 const numberOfSatelliteToEarth = 1;
-//variables decalred using const can neither be re-aasinged not values can be re-decalred
+//variables decalred using const can neither be re-aasinged nor the values can be re-decalred
 
 
-const rulesToDecalreVariables = "yes syntax must be follwed";
+const rulesToDecalreVariables = "yes, must be follwed";
 
 const ruleNo1 = "Variable name cannot start with a number, can end with a number";
-const ruleNo2 = "Variable name cannot start with symbol, Exception : $ and _ can be used ";
+const ruleNo2 = "Variable name cannot start with symbol, Exception : $ and _ can be used";
 const ruleNo3 = "Variable name cannot contain spaces";
 const ruleNo4 = "Variable name is case sensetive";
 const ruleNo5 = "Variable name cannot be a reserved keyword, Example: let, while, true, and many more"
 
+
+//variable can also be "Boolean"
 let iLearnedAboutVariables = true;
-//variables can also be true or false 
+
 
 
 
