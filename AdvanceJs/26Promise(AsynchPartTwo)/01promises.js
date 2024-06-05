a230212ea5d1a7c1cc8fc338c2f2e56eef6c392b
Ramakrishna Kumbar <ramlikestocode@gmail.com>
2024-06-05 23:12:50 +0530

    creating Promise

---
+++ AdvanceJs/26Promise(AsynchPartTwo)/01promises.js
@@ -0,0 +1,53 @@
+/* In JavaScript, promises are objects used to handle asynchronous operations. 
+They represent a value that may be available now, or in the future, or never. 
+Promises are widely used in modern JavaScript for handling asynchronous tasks,
+like fetching data from a server, reading files, or any operation that might take some time to complete. */
+
+const promiseOne = new Promise( (resolve, reject) => {
+    setTimeout(() => {
+    console.log("Asynchronous Task 1");
+    resolve()
+   }, 1000) 
+});
+
+promiseOne.then(function(){
+    console.log("Promise consumed");
+});
+
+new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log("Asynchronous Task 2");
+    }, 1000);
+
+}).then(() => {
+    console.log("Asynchronous Resolved");
+});
+
+const promiseThree = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve({username: "Ram", email:"ramlikestocode@gmail.com"})
+    }, 1000)
+});
+
+promiseThree.then((userData) => {
+    console.log(userData);
+});
+
+const promiseFour = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        let error = true
+        if (!error) {
+            resolve({codingLanguage: "javascript",
+                courseDurationInMonths : 8
+            });
+        }else {
+            reject("Something went wrong")
+        }
+    }, 1000)
+})
+
+promiseFour.then((userData) => {
+    console.log(userData);
+}).then(() => {
+
+});
