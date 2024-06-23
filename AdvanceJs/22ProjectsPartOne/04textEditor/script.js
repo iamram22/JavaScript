4259b2a28f15039ecb0b00481ed09a1657d27465
Ramakrishna Kumbar <ramlikestocode@gmail.com>
2024-06-23 22:11:43 +0530

    creating a small text editor

--- AdvanceJs/22ProjectsPartOne/04textEditor/script.js
+++ AdvanceJs/22ProjectsPartOne/04textEditor/script.js
@@ -1,13 +1,33 @@
-let uC = document.querySelector(".uppercase")
- const lC = document.querySelector(".lowercase")
- const capt = document.querySelector(".capitalize")
- const bold = document.querySelector(".bold")
- const italic = document.querySelector(".italic")
- const uL = document.querySelector(".underline")
- const btn = document.querySelector(".btn")
- let output = document.getElementById("output-field")
- let ip = document.getElementById("input-field")
+const uC = document.querySelector('.uppercase');
+const lC = document.querySelector('.lowercase');
+const capt = document.querySelector('.capitalize');
+const bold = document.querySelector('.bold');
+const italic = document.querySelector('.italic');
+const uL = document.querySelector('.underline');
+const btn = document.querySelector('.btn');
 
-uC.addEventListener("click", function(){
-  ip.style.textTransform = "uppercase"
-})
+let output = document.getElementById('output-field');
+
+let ip = document.getElementById('input-field');
+
+uC.addEventListener('click', function () {
+  ip.style.textTransform = 'uppercase';
+});
+lC.addEventListener('click', function () {
+  ip.style.textTransform = 'lowercase';
+});
+capt.addEventListener('click', function () {
+  ip.style.textTransform = 'capitalize';
+});
+bold.addEventListener('click', function () {
+  ip.style.fontWeight = 'bold';
+});
+italic.addEventListener('click', function () {
+  ip.style.fontStyle = 'italic';
+});
+uL.addEventListener('click', function () {
+  ip.style.textDecoration = 'underline';
+});
+
+output.innerHTML =
+  'Available edits : uppercase, italic, underline, lowecase, capitalize ';
